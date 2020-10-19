ebadb5f520d9fb5af30bec69bcb416f05a78895f
Alexey Anisimov <vkrotin@gmail.cpm>
2020-10-19 11:22:37 +0500

    Initial Commit

---
+++ PatternExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PatternExample
+//
+//  Created by  vkrotin on 19.10.2020.
+//  Copyright © 2020  vkrotin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
